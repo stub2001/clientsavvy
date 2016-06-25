07d3e9dd47d70701ec46b3ee40e70a39c4b23c97
Stuart Brown <stuart.brown@cohaesus.co.uk>
2016-06-25 15:21:30 +0100

    Set up initial gulp & inuit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+
+//browser Sync
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('src/css/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/css/*.scss', ['sass']);
+    gulp.watch('*.html', browserSync.reload); 
+ //  gulp.watch('app/js/**/*.js', browserSync.reload); // configure JS watcher
+});
+
+
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
